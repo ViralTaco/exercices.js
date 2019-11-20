de1a44934c5dce96a8a3a761089da9345c0646e9
ViralTaco <7462994+ViralTaco@users.noreply.github.com>
2019-11-20 18:47:20 +0100

    onsubmit script

---
+++ 08_form_google/scripts/onsubmit.js
@@ -0,0 +1,13 @@
+function validate(elementEvent) {
+  var inputElement = elementEvent.srcElement
+  var text = inputElement.q.value
+  
+  if (!text) {
+    elementEvent.preventDefault() // don't run action on function call
+    alert('Empty form')
+  } 
+}
+
+document.getElementById('monform').onsubmit = function () {
+  validate(event)
+}
